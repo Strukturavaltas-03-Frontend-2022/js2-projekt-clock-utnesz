cf4390752226f7ff63c85e314b32f4a5e2449995
utnesz <90829509+utnesz@users.noreply.github.com>
2022-08-25 17:41:27 +0200

    Version 1.0

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+const showTime = () => {
+    const time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+
+    document.getElementById("hours").innerHTML = hour;
+    document.getElementById("min").innerHTML = min;
+    document.getElementById("sec").innerHTML = sec; 
+    setInterval(showTime, 1000)
+}
+
+showTime();
